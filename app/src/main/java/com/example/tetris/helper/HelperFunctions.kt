7b718d7134f1941507436d1ef36f1eac29498ef4
Vitaliy Novogran <vi19954@gmail.com>
2022-05-14 01:41:07 +0300

    Add some Tetris functional classes

---
+++ app/src/main/java/com/example/tetris/helper/HelperFunctions.kt
@@ -0,0 +1,4 @@
+package com.example.tetris.helper
+
+fun array2dOfByte(sizeOuter: Int, sizeInner: Int): Array<ByteArray>
+    = Array(sizeOuter){ByteArray(sizeInner)}
---
+++ app/src/main/java/com/example/tetris/models/Shape.kt
@@ -0,0 +1,20 @@
+package com.example.tetris.models
+
+import java.lang.IllegalArgumentException
+
+enum class Shape(val frameCount: Int, val startPosition: Int) {
+    Tetromino(2,2) {
+        override fun getFrame(frameNumber: Int): Frame{
+            return when (frameNumber) {
+                0 -> Frame(4).addRow("1111")
+                1 -> Frame(1)
+                    .addRow("1")
+                    .addRow("1")
+                    .addRow("1")
+                else -> throw IllegalArgumentException("$frameNumber is an invalid frame number.")
+            }
+        }
+    };
+
+    abstract fun getFrame(frameNumber: Int): Frame
+}
